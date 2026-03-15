ced150141ab1d7a6a71fe0a618a3135c54c54247
AbdelkaderBarhoumi21 <abdelkaderbarhoumi21@gmail.com>
2026-03-15 14:04:11 +0100

    feat: add message model

---
+++ lib/features/messages/widgets/custom_message_tile.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class CustomMessageTile extends StatelessWidget {
+  const CustomMessageTile({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: [],
+    );
+  }
+}
