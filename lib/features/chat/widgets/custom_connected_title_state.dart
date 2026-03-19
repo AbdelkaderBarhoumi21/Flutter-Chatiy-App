aa0ed678dfe74837003fb10a66864173fcdfd0d3
AbdelkaderBarhoumi21 <abdelkaderbarhoumi21@gmail.com>
2026-03-19 12:19:47 +0100

    feat: add connectted title state widgets

--- lib/features/chat/widgets/custom_connected_title_state.dart
+++ lib/features/chat/widgets/custom_connected_title_state.dart
@@ -1,5 +1,8 @@
+import 'package:collection/collection.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_chatiy_app/core/extension/app_get_stream_extension.dart';
 import 'package:flutter_chatiy_app/features/chat/widgets/custom_typing_indicator.dart';
+import 'package:jiffy/jiffy.dart';
 import 'package:stream_chat_flutter_core/stream_chat_flutter_core.dart';
 
 class CustomConnectedTitleState extends StatelessWidget {
@@ -8,6 +11,43 @@ class CustomConnectedTitleState extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return CustomTypingIndicator();
+    Widget? alternativeWidget;
+    final channel = context.channel;
+    final memberCount = channel.memberCount;
+    if (memberCount != null && memberCount > 2) {
+      var text = 'Members: $memberCount';
+      final watcherCount = channel.state?.watcherCount ?? 0;
+      if (watcherCount > 0) {
+        text = 'watchers $watcherCount';
+      }
+      alternativeWidget = Text(text);
+    } else {
+      final userId = context.currentUser?.id;
+      final otherMember = members?.firstWhereOrNull((e) => e.userId != userId);
+      if (otherMember != null) {
+        if (otherMember.user?.online == true) {
+          alternativeWidget = const Text(
+            'Online',
+            style: TextStyle(
+              fontSize: 10,
+              fontWeight: FontWeight.bold,
+              color: Colors.green,
+            ),
+          );
+        } else {
+          alternativeWidget = Text(
+            'Last online: '
+            '${Jiffy.parseFromDateTime(otherMember.user?.lastActive ?? DateTime.now()).fromNow()}',
+            style: const TextStyle(
+              fontSize: 10,
+              fontWeight: FontWeight.bold,
+              color: Colors.red,
+            ),
+          );
+        }
+      }
+    }
+
+    return CustomTypingIndicator(alternativeWidget: alternativeWidget);
   }
 }
